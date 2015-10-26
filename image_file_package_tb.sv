acce78585b2aaf4056e2d084cc955214cf8a2101
Young <wmy367@gmail.com>
2015-10-26 11:55:00 +0800

    Modify something to debug

--- image_file_package_tb.sv
+++ image_file_package_tb.sv
@@ -1,27 +1,21 @@
-/*******************************************************
-______________                ______________
-______________ \  /\  /|\  /| ______________
-______________  \/  \/ | \/ | ______________
-
---Module Name:
---Project Name:
---Chinese Description:
-	
---English Description:
-	
---Version:VERA.1.0.0
---Data modified:
---author:Young-ÎâÃ÷
---E-mail: wmy367@Gmail.com
---Data created:
-________________________________________________________
-********************************************************/
+/**********************************************
+______________________________________________
+_______  ___   ___          ___   __   _    _
+_______ |     |   | |\  /| |___  |  \  |   /_\
+_______ |___  |___| | \/ | |___  |__/  |  /   \
+_______________________________________________
+descript:
+author : Young
+Version: VERA.0.0
+creaded: 2015/6/9
+madified:
+***********************************************/
 `timescale 1ns/1ps
 module image_file_package_tb;
 
 import ImageFilePkg::*;
 
-ImageFileClass afile	= new("tmp.txt");
+ImageFileClass afile	= new("E:/work/video_process_module/image_file_class/tmp.txt");
 
 int line [$];
 
